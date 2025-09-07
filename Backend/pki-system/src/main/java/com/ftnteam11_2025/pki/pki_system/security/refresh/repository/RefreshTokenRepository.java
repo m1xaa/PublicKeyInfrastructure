7b7759e738bf805f09af203bcfc2b6812f18adcc
Stefan Stjepic <stefan.stjepic03@gmail.com>
2025-09-07 18:32:35 +0200

    refactore(auth): Allow user to have multiple refresh tokens

--- Backend/pki-system/src/main/java/com/ftnteam11_2025/pki/pki_system/security/refresh/repository/RefreshTokenRepository.java
+++ Backend/pki-system/src/main/java/com/ftnteam11_2025/pki/pki_system/security/refresh/repository/RefreshTokenRepository.java
@@ -9,5 +9,4 @@ import java.util.Optional;
 
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long>, JpaSpecificationExecutor<RefreshToken> {
     Optional<RefreshToken> findByToken(String token);
-    Optional<RefreshToken> findByUser(User user);
 }
